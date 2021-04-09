6e6e668260b95dd231f56adbb83736e5af1daa93
Yepremyan <yeranikoshamo@gmail.com>
2021-04-09 21:18:26 +0400

    added QuickSort experiment

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -4,6 +4,7 @@ import expirements.executor.ExperimentExecutor;
 import expirements.sorting.BubbleSortExperiment;
 import expirements.sorting.InsertionSortExperiment;
 import expirements.sorting.MergeSortExperiment;
+import expirements.sorting.QuickSortExperiment;
 import expirements.sorting.SelectionSortExperiment;
 
 public class MainApplication {
@@ -14,6 +15,7 @@ public class MainApplication {
             .add(new SelectionSortExperiment(Arrays.asList(-5, 8, 6, 8, 0, 8, -10, 0, -8, 5, 15, 10)))
             .add(new InsertionSortExperiment(Arrays.asList(-5, 8, 6, 8, 0, 8, -10, 0, -8, 5, 15, 10)))
             .add(new MergeSortExperiment(Arrays.asList(-5, 8, 6, 8, 0, 8, -10, 0, -8, 5, 15, 10)))
+            .add(new QuickSortExperiment(Arrays.asList(-5, 8, 6, 8, 0, 8, -10, 0, -8, 5, 15, 10)))
             .startAll();
     }
 }
